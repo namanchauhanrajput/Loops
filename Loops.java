7047e58bb235ef4ca564cef4f602ccd70749329a
Naman Chauhan <namanrajputnn001@gmail.com>
2024-10-16 15:27:58 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,29 @@
+import com.sun.source.tree.WhileLoopTree;
+
+public class Loops {
+//For Loop
+    public static void main(String args[]) {
+
+       // for (int i = 0; i < 11; i++) {
+         //   System.out.println(i);
+        //}
+
+       // WhileLoop
+     //   int i = 0;
+       // while (i <11) {
+         //   System.out.println(i);
+           // i = i + 1; // i++;
+       // }
+
+        // do whileloop
+
+        int i = 0;
+        do {
+            System.out.println(i +" ");
+            i++;
+        }
+        while (i < 11);
+
+
+    }
+}
